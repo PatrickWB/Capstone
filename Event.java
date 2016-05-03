475207e842ec9e23b23edf3b0817aa10edd7cfa1
Patrick Bartholomew <patrick.bartholomew.jr@gmail.com>
2016-05-02 20:30:46 -0500

    Added files via upload

--- Event.java
+++ Event.java
@@ -8,10 +8,11 @@
  * Author:      Kaitlin Russ
  * Date:        2/25/2016
  * Description: Form that allows users to create a group in Unite.
+ * Modified by: Heather Guyll
+ * Date:        3/01/2016
  *
  */
 package secure.unite.entities;
-
 
 import java.util.Date;
 import java.sql.Time;
@@ -27,13 +28,16 @@ import org.primefaces.event.SelectEvent;
 import unite.DAO.EventDAO;
 import unite.DAO.SignupDAO;
 
-@ManagedBean
+@ManagedBean(name="eventBean")
 @ViewScoped
 public class Event {
-
     // group creation input variables
-    private int eGroup;
-    private int eId;
+    
+    
+    //private int eGroup;
+    //private int eId;
+    private int eventGroup;
+    private int eventID;
     private String eventname;
     private String eventNeedStatus;
     private String eventdesc;
@@ -47,128 +51,26 @@ public class Event {
     private String startdatetime;
     private String enddatetime;
     private Timestamp startTime;
-    private Timestamp endTime;
-    // Getters -- Setters
-    public String getEventname() {
-        return eventname;
-    }
+    private Timestamp endTime;        
     
-    public void setEventname(String eventname) {
-        this.eventname = eventname;
-    }
-
-    public String getEventdesc() {
-        return eventdesc;
-    }
-    public void seteGroup(int Egroup){
-      this.eGroup = Egroup;
-    }
-    public int geteGroup(){
-        return eGroup;
-    }
-    public void setEventdesc(String eventdesc) {
-        this.eventdesc = eventdesc;
-    }
-    
-    public String getEventdate() {
-        return eventdate;
-    }
- 
-    public void setEventdate(String eventdate) {
-        this.eventdate = eventdate;
-    }
-    
-    public String getEventstart() {
-        return eventstart;
-    }
- 
-    public void setEventstart(String eventstart) {
-        this.eventstart = eventstart;
-    }
-    
-    public String getEventend() {
-        
-        return eventend;
-    }
- 
-    public void setEventend(String eventend) {
-        this.eventend = eventend;
-    }
-    
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public String getZip() {
-        return zip;
-    }
-
-    public void setZip(String zip) {
-        this.zip = zip;
-    }
-    
-    public String getStartDate () {
-        return startdatetime;
-    }
-    
-    public void setStartDate(String evendate, String eventstart) {
-        this.eventdate = eventdate;
-        this.eventstart = eventstart;
-        startdatetime = eventdate + " " + eventstart;
-    }
-    
-    public String getEndDate(){
-        return enddatetime;
-    }
-    
-    public void setEndDate(String evendate, String eventend) {
-        this.eventdate = eventdate;
-        this.eventend = eventend;
-        enddatetime = eventdate + " " + eventstart;
-    }
-    
-    /*private static java.sql.Date convertUtilToSql(java.util.Date uDate) {
-        java.sql.Date newdate = new java.sql.Date(uDate.getTime());
-        return newdate;
-    } */
-
     // get data from groupsignup page.
+    
     public String setupNewEvent() {
         FacesContext context = FacesContext.getCurrentInstance();
-        System.out.println(startdatetime);
-        System.out.println(enddatetime);
-        boolean valid = EventDAO.CreateEvent(eventname, eventdesc, startdatetime, enddatetime, eventNeedStatus, address, city, state, zip);
+        System.out.println(getStartdatetime());
+        System.out.println(getEnddatetime());
+        boolean valid = EventDAO.CreateEvent(getEventname(), getEventdesc(), getStartdatetime(), getEnddatetime(), getEventNeedStatus(), getAddress(), getCity(), getState(), getZip());
 
-            if (valid) {                
-                // let user know their account was created.                
-                context.addMessage(null, new FacesMessage("Event Created", "Event Created Successfully"));
-                return "success";
-            } else {
-                // don't redirect.  show the user an error message.
-                context.addMessage(null, new FacesMessage("Event Creation Failed", "Missing Information"));
-                System.out.println("Missing Information");
-                return "failure";
-            }
+        if (valid) {
+            // let user know their account was created.                
+            context.addMessage(null, new FacesMessage("Event Created", "Event Created Successfully"));
+            return "success";
+        } else {
+            // don't redirect.  show the user an error message.
+            context.addMessage(null, new FacesMessage("Event Creation Failed", "Missing Information"));
+            System.out.println("Missing Information");
+            return "failure";
+        }
     }
 
     public static String Cancel() {
@@ -176,60 +78,133 @@ public class Event {
         return "cancel";
     }
 
-    /**
-     * @return the eventNeedStatus
-     */
+    public String getEventname() {
+        return eventname;
+    }
+
     public String getEventNeedStatus() {
         return eventNeedStatus;
     }
 
-    /**
-     * @param eventNeedStatus the eventNeedStatus to set
-     */
-    public void setEventNeedStatus(String eventNeedStatus) {
-        this.eventNeedStatus = eventNeedStatus;
+    public String getEventdesc() {
+        return eventdesc;
     }
 
-    /**
-     * @return the startTime
-     */
+    public String getEventdate() {
+        return eventdate;
+    }
+
+    public String getEventstart() {
+        return eventstart;
+    }
+
+    public String getEventend() {
+        return eventend;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public String getStartdatetime() {
+        return startdatetime;
+    }
+
+    public String getEnddatetime() {
+        return enddatetime;
+    }
+
     public Timestamp getStartTime() {
         return startTime;
     }
 
-    /**
-     * @param startTime the startTime to set
-     */
-    public void setStartTime(Timestamp startTime) {
-        this.startTime = startTime;
-    }
-
-    /**
-     * @return the endTime
-     */
     public Timestamp getEndTime() {
         return endTime;
     }
 
-    /**
-     * @param endTime the endTime to set
-     */
+    public void setEventname(String eventname) {
+        this.eventname = eventname;
+    }
+
+    public void setEventNeedStatus(String eventNeedStatus) {
+        this.eventNeedStatus = eventNeedStatus;
+    }
+
+    public void setEventdesc(String eventdesc) {
+        this.eventdesc = eventdesc;
+    }
+
+    public void setEventdate(String eventdate) {
+        this.eventdate = eventdate;
+    }
+
+    public void setEventstart(String eventstart) {
+        this.eventstart = eventstart;
+    }
+
+    public void setEventend(String eventend) {
+        this.eventend = eventend;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public void setStartdatetime(String startdatetime) {
+        this.startdatetime = startdatetime;
+    }
+
+    public void setEnddatetime(String enddatetime) {
+        this.enddatetime = enddatetime;
+    }
+
+    public void setStartTime(Timestamp startTime) {
+        this.startTime = startTime;
+    }
+
     public void setEndTime(Timestamp endTime) {
         this.endTime = endTime;
     }
 
-    /**
-     * @return the eId
-     */
-    public int geteId() {
-        return eId;
+    public int getEventGroup() {
+        return eventGroup;
     }
 
-    /**
-     * @param eId the eId to set
-     */
-    public void seteId(int eId) {
-        this.eId = eId;
+    public int getEventID() {
+        return eventID;
     }
+
+    public void setEventGroup(int eventGroup) {
+        this.eventGroup = eventGroup;
+    }
+
+    public void setEventID(int eventID) {
+        this.eventID = eventID;
+    }
+
 
 }
